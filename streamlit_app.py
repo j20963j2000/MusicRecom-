87448d68d798ef2e29320107f34b09500b5f29af
Jackson <j20963j2000@yahoo.com.tw>
2022-01-05 21:00:57 +0800

    Update streamlit_app.py
    test graph code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,39 @@
 
 import streamlit as st
 from rdflib import Graph
-from SPARQLWrapper import SPARQLWrapper
 from streamlit_agraph import agraph, Node, Edge, Config, TripleStore
 
+nodes = []
+edges = []
+nodes.append( Node(id="Spiderman", 
+                   label="Peter Parker", 
+                   size=400, 
+                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
+            ) # includes **kwargs
+nodes.append( Node(id="Captain_Marvel", 
+                   size=400, 
+                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
+            )
+edges.append( Edge(source="Captain_Marvel", 
+                   label="friend_of", 
+                   target="Spiderman", 
+                   type="CURVE_SMOOTH") 
+            ) # includes **kwargs
+
+config = Config(width=500, 
+                height=500, 
+                directed=True,
+                nodeHighlightBehavior=True, 
+                highlightColor="#F7A7A6", # or "blue"
+                collapsible=True,
+                node={'labelProperty':'label'},
+                link={'labelProperty': 'label', 'renderLabel': True}
+                # **kwargs e.g. node_size=1000 or node_color="blue"
+                ) 
+
+return_value = agraph(nodes=nodes, 
+                      edges=edges, 
+                      config=config)
 
 graph = Graph()
 graph.parse("http://www.w3.org/People/Berners-Lee/card")
