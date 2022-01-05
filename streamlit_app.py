25470f1b88a327476957be0b39e9f7ccc66aca40
Jackson <j20963j2000@yahoo.com.tw>
2022-01-05 20:57:14 +0800

    test
    test graph code and modify requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,14 @@
 
 import streamlit as st
 from SPARQLWrapper import SPARQLWrapper
-from streamlit_agraph import agraph, Node, Edge, Config
+from streamlit_agraph import agraph, Node, Edge, Config, TripleStore
 
 
-nodes = []
-edges = []
-nodes.append( Node(id="Spiderman", 
-                   label="Peter Parker", 
-                   size=800, 
-                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
-            ) # includes **kwargs
-nodes.append( Node(id="Captain_Marvel", 
-                   size=800, 
-                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
-            )
-edges.append( Edge(source="Captain_Marvel", 
-                   label="friend_of", 
-                   target="Spiderman", 
-                   type="CURVE_SMOOTH") 
-            ) # includes **kwargs
+graph = Graph()
+graph.parse("http://www.w3.org/People/Berners-Lee/card")
+store = TripleStore()
 
-config = Config(width=500, 
-                height=500, 
-                directed=True,
-                nodeHighlightBehavior=True, 
-                highlightColor="#F7A7A6", # or "blue"
-                collapsible=True,
-                node={'labelProperty':'label'},
-                link={'labelProperty': 'label', 'renderLabel': True}
-                # **kwargs e.g. node_size=1000 or node_color="blue"
-                ) 
-
-return_value = agraph(nodes=nodes, 
-                      edges=edges, 
-                      config=config)
+for subj, pred, obj in graph:
+    store.add_triple(subj, pred, obj, "")
+    
+agraph(list(store.getNodes()), list(store.getEdges()), config)
