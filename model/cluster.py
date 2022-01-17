a83331f5f2e2655c6128597d723e0da344c665de
vic4code <vic4code@gmail.com>
2022-01-17 19:51:28 +0800

    Add model

---
+++ model/cluster.py
@@ -0,0 +1,45 @@
+import os
+import numpy as np
+import pandas as pd
+
+import seaborn as sns
+import plotly.express as px 
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.metrics import euclidean_distances
+from scipy.spatial.distance import cdist
+
+import warnings
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv("data/data.csv")
+genre_data = pd.read_csv('data/data_by_genres.csv')
+year_data = pd.read_csv('data/data_by_year.csv')
+
+print(data.info())
+print(genre_data.info())
+print(genre_data.info())
+print(year_data.info())
+
+from yellowbrick.target import FeatureCorrelation
+
+feature_names = ['acousticness', 'danceability', 'energy', 'instrumentalness',
+       'liveness', 'loudness', 'speechiness', 'tempo', 'valence','duration_ms','explicit','key','mode','year']
+
+X, y = data[feature_names], data['popularity']
+
+# Create a list of the feature names
+features = np.array(feature_names)
+
+# Instantiate the visualizer
+visualizer = FeatureCorrelation(labels=features)
+
+plt.rcParams['figure.figsize']=(20,20)
+visualizer.fit(X, y)     # Fit the data to the visualizer
+visualizer.show()
