1753e7e31b859c00e6658345e1de56d060af302b
Jackson <j20963j2000@yahoo.com.tw>
2022-01-23 02:17:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ if st.sidebar.button("Go Explore !"):
               with col2:
                      col2.write("Bruno Mars")
                      col2.write("24k Magics")
-                     button2 = st.button("Listening on Spotify", key=1)
+                     button2 = st.button("Listening on Spotify", key=2)
                      if button2:
                             js = "window.open('https://www.streamlit.io/')"  # New tab or window
                             js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
