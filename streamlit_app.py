a8d10493bb8bc9d6f19698506fffae230fa84f3c
Jackson <j20963j2000@yahoo.com.tw>
2022-01-22 23:02:04 +0800

    layouts design
    adding sidebar and base layouts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,14 @@
-
 import streamlit as st
-from rdflib import Graph
-from streamlit_agraph import agraph, Node, Edge, Config, TripleStore
 
-nodes = []
-edges = []
-nodes.append( Node(id="Spiderman", 
-                   label="Peter Parker", 
-                   size=400, 
-                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png") 
-            ) # includes **kwargs
-nodes.append( Node(id="Captain_Marvel", 
-                   size=400, 
-                   svg="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png") 
-            )
-edges.append( Edge(source="Captain_Marvel", 
-                   label="friend_of", 
-                   target="Spiderman", 
-                   type="CURVE_SMOOTH") 
-            ) # includes **kwargs
+st.title("MusicRecom")
 
-config = Config(width=500, 
-                height=500, 
-                directed=True,
-                nodeHighlightBehavior=True, 
-                highlightColor="#F7A7A6", # or "blue"
-                collapsible=True,
-                node={'labelProperty':'label'},
-                link={'labelProperty': 'label', 'renderLabel': True}
-                # **kwargs e.g. node_size=1000 or node_color="blue"
-                ) 
+st.write("""
+# MusicRecom would help you to explore different songs based on your preferences
 
-return_value = agraph(nodes=nodes, 
-                      edges=edges, 
-                      config=config)
+""")
+
+
+"""
+Sidebar for user input 
+"""
+st.sidebar.header("Give us some infors !")
