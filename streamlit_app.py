f34d2c6bb7965416e556740a665f91914119e906
Jackson <j20963j2000@yahoo.com.tw>
2022-01-22 23:03:39 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,8 @@ import streamlit as st
 st.title("MusicRecom")
 
 st.write("""
-## MusicRecom would help you to explore different songs based on your preferences
+#### MusicRecom would help you to explore different songs based on your preferences
 
 """)
 
-
-"""
-Sidebar for user input 
-"""
 st.sidebar.header("Give us some infors !")
