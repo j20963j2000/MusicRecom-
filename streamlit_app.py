7f82e1aa6c4784a9287c7419c8663f7022adb806
Jackson <j20963j2000@yahoo.com.tw>
2022-01-22 23:10:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 st.title("MusicRecom")
@@ -8,3 +9,17 @@ st.write("""
 """)
 
 st.sidebar.header("Give us some infors !")
+
+
+def user_input_features():
+       artist = st.sidebar.text_input("Which artist you like the most ?", "Bruno Mars")
+       data = {"artist":artist}
+       features = pd.DataFrame(data, index=[0])
+       return features
+
+df = user_input_features()
+
+user_preference_artist = df["artist"]
+
+if df:
+       st.write("We explore songs based on :", user_input_features)
